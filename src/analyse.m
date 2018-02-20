636ac54fc5fabcf2e78b9d51471e09b2f45d3b7e
Tomas Jilek <jilekt@feec.vutbr.cz>
2018-02-20 22:21:13 +0100

    Documentation updated.

--- src/analyse.m
+++ src/analyse.m
@@ -8,13 +8,14 @@ M = csvread(filename, 10, 0);
 % parsing
 % data: 142249.04,4913.63631506,01634.53113072,-0006.849,-0008.292,-0000.558,0.26,47.268,0.00,0.00,0,0,9676179,2010996,-19079139,-15290034
 utc = mod(M(:, 1), 100) + mod(fix(M(:, 1) / 100), 100) * 60 + fix(M(:, 1) / 10000) * 3600; % [s]
-n = M(:, 4);
-e = M(:, 5);
-u = M(:, 6);
-v = M(:, 7);
-a = M(:, 8);
-
-t = utc - utc(1);
+lat = M(:, 2);
+lon = M(:, 3);
+n   = M(:, 4);
+e   = M(:, 5);
+u   = M(:, 6);
+v   = M(:, 7) / 3.6;  % [km/h] -> [m/s]
+a   = M(:, 8);
+t   = utc - utc(1);
 
 % === GRAPHS ===
 figure(1)
@@ -23,11 +24,14 @@ clf
 subplot(4, 1, 1)
 plot(e, n)
 axis equal
+xlabel('e [m]')
+ylabel('n [m]')
 grid on
 
 subplot(4, 1, 2)
 plot(t, e)
 xlabel('time [s]')
+ylabel('e [m]')
 grid on
 
 subplot(4, 1, 3)
@@ -39,5 +43,5 @@ grid on
 subplot(4, 1, 4)
 plot(t, v)
 xlabel('time [s]')
-ylabel('horizontal speed [km/s]')
+ylabel('horizontal speed [m/s]')
 grid on
